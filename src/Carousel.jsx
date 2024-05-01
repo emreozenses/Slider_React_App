e30745a064fe3d186378cfdb34bc34f038efe6d8
Emre OZENSES <emreozenses@gmail.com>
2024-05-01 16:12:18 +0300

    caoursel next and prev buttons functions written

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,29 +3,45 @@ import { shortList,list,longList } from "./data"
 import { GrFormNext } from 'react-icons/gr'
 import { GrFormPrevious } from 'react-icons/gr'
 import { IoChevronBack } from 'react-icons/io5'
+import {FaQuoteRight} from 'react-icons/fa'
 
 
 import React from 'react'
 
 const Carousel = () => {
+
+    const prevSlide=()=>{};
+    const nextSlide = () => {};
+
     const [peopleList,setPeopleList] = useState(shortList);
     console.log(list);
     const [index,setIndex] = useState(0);
   return (
     <section className="slider-container">
       {peopleList.map((person) => {
-          const { id, image, title, name, quote } = person
-          return (
-              <div className="slide" key={id}>
+        const { id, image, title, name, quote } = person
+        return (
+          <article className="slide" key={id}>
             <img src={image} alt={name} className="person-img" />
             <h3 className="name">{name}</h3>
             <h5 className="title">{title}</h5>
             <p className="text">{quote}</p>
-          </div>
+            <FaQuoteRight className="icon" />
+          </article>
         )
-    })}
-    <GrFormPrevious className="prev"/>
-      <GrFormNext className="next" />
+      })}
+      <button 
+      type="button" 
+      className="prev"
+      onClick={prevSlide}>
+        <GrFormPrevious />
+      </button>
+      <button 
+      type="button" 
+      className="next"
+      onClick={nextSlide}>
+        <GrFormNext />
+      </button>
     </section>
   )
 }
