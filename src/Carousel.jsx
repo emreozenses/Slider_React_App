49d62f580db0bff5e38597e76ca0060a94b2984a
Emre OZENSES <emreozenses@gmail.com>
2024-05-02 17:41:55 +0300

    slider all functions activated

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -10,17 +10,28 @@ import React from 'react'
 
 const Carousel = () => {
 
-    const prevSlide=()=>{};
-    const nextSlide = () => {};
-
-    const [peopleList,setPeopleList] = useState(list);
+    const prevSlide=()=>{
+            setCurrentPerson((oldPerson) => {
+              const result = [oldPerson - 1+peopleList.length] % peopleList.length
+              return result
+            })
+    };
+    const nextSlide = () => {
+      setCurrentPerson((oldPerson)=>{
+        const result = [oldPerson+1]%peopleList.length
+      return result;
+      })
+    };
+    const [currentPerson,setCurrentPerson]=useState(0);
+    const [peopleList,setPeopleList] = useState(longList);
 
   return (
     <section className="slider-container">
       {peopleList.map((person,personIndex) => {
         const { id, image, title, name, quote } = person
         return (
-          <article className="slide" style={{transform:`translateX(${100*personIndex}%)`}} key={id}>
+          <article className="slide" style={{
+            transform:`translateX(${100*(personIndex-currentPerson)}%)`,opacity:`${personIndex===currentPerson?1:0}`,visibility:`${personIndex===currentPerson?'visible':'hidden'}`}} key={id}>
             <img src={image} alt={name} className="person-img" />
             <h3 className="name">{name}</h3>
             <h5 className="title">{title}</h5>
